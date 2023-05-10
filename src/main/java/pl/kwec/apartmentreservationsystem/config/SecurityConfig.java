1c7927540f73b59a837c6cdef90e59465b24be4c
barto <bartosz.kwec@vp.pl>
2023-05-10 16:30:29 +0200

    The problem with spring security

---
+++ src/main/java/pl/kwec/apartmentreservationsystem/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package pl.kwec.apartmentreservationsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    protected void configure(HttpSecurity http) throws Exception{
+
+
+    }
+
+}
